8a422c32f6e945c44b3ff7a1346db10731b5f278
Avinash Maurya <akmaurya31@gmail.com>
2020-11-19 19:09:28 +0530

    Update customer.routes.js

--- app/routes/customer.routes.js
+++ app/routes/customer.routes.js
@@ -2,6 +2,6 @@ module.exports = app => {
   const customers = require("../controllers/customer.controller.js");
  
     // By Ashish Ji 
-    app.post("/changePbank",customers.changePbank);
+    app.get("/changePbank",customers.changePbank);
     
 };
